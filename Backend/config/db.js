323a67a5963086b37f3ad23d2542a00273c5fd89
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-11-24 15:23:18 +0530

    Updated

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://Kushan_db_user:Quiz#32@cluster0.lubavlg.mongodb.net/?appName=Cluster0')   
+    await mongoose.connect('mongodb+srv://Kushan_db_user:Quiz32@cluster0.lubavlg.mongodb.net/?appName=Cluster0')   
     .then(() => {
         console.log("Connected to MongoDB");
     })
